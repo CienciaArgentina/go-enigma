851e49695f7154f30fb55a16ffaf9bac672f5df9
Nico <nicolasecamilo@gmail.com>
2020-01-31 22:38:42 -0300

    Added Config constructor

--- conf/config.go
+++ conf/config.go
@@ -1,16 +1,53 @@
 package conf
 
-import "syscall"
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
+const (
+	SCOPE = "SCOPE"
+	GOENVIRONMENT = "GO_ENVIRONMENT"
+	Production = "production"
+	Testing = "testing"
+	Local = "local"
+
+)
 
 type Configuration struct {
 	Database struct {
-		Username string
-		Password string
-		Host string
-		Database string
-	}
+		User string `yaml:user`
+		Password string `envconfig:DB_PASSWORD`
+		Host string `envconfig:DB_HOST`
+		Database string `yaml:dbname`
+	} `yaml:database`
 }
 
 func New() (*Configuration, error) {
+	config := &Configuration{}
+	err := envconfig.Process("db", &config)
+	if err != nil {
+		return nil, err
+	}
 
+	scope := os.Getenv(SCOPE)
+	if scope == "" {
+		scope = Local
+	}
+
+	data, err := os.Open(fmt.Sprintf("config.%s.yml", scope))
+	if err != nil {
+		return nil, err
+	}
+
+	defer data.Close()
+
+	decoder := yaml.NewDecoder(data)
+	if err := decoder.Decode(config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/CienciaArgentina/go-enigma
 
 go 1.13
+
+require github.com/kelseyhightower/envconfig v1.4.0
