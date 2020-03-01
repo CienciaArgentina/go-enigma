e90f6944ea78a9ad4156f5aed70dfcf7669b197b
Nico <nicolasecamilo@gmail.com>
2020-03-01 03:15:24 -0300

    Resend email confirmation tests

--- internal/recovery/service_test.go
+++ internal/recovery/service_test.go
@@ -11,6 +11,7 @@ import (
 const (
 	GetEmailByUserId = "GetEmailByUserId"
 	ConfirmUserEmail = "ConfirmUserEmail"
+	GetuserIdByEmail = "GetuserIdByEmail"
 )
 
 type RecoveryRepositoryMock struct {
@@ -25,6 +26,11 @@ func (r *RecoveryRepositoryMock) GetEmailByUserId(userId int64) (string, *UserEm
 func (r *RecoveryRepositoryMock) ConfirmUserEmail(email string, token string) error {
 	args := r.Called(email, token)
 	return args.Error(0)
+}
+
+func (r *RecoveryRepositoryMock) GetuserIdByEmail(email string) (int64, error) {
+	args := r.Called(email)
+	return args.Get(0).(int64) ,args.Error(1)
 }
 
 func GetServiceAndMock() (Service, *RecoveryRepositoryMock) {
@@ -102,4 +108,31 @@ func TestConfirmEmailShouldThrowNoError(t *testing.T) {
 	confirm, err := svc.ConfirmEmail("asd", "asd")
 	require.True(t, confirm)
 	require.Nil(t, err)
+}
+
+func TestResendEmailConfirmationEmailShouldReturnErrorWhenEmailisNil(t *testing.T) {
+	svc, _ := GetServiceAndMock()
+	sent, err := svc.ResendEmailConfirmationEmail("")
+	require.False(t, sent)
+	require.Equal(t, config.ErrEmailValidationFailed, err)
+}
+
+func TestResendEmailConfirmationEmailShouldReturnErrorWhenGetUserIdByEmailFails(t *testing.T) {
+	svc, mock := GetServiceAndMock()
+	mock.On(GetuserIdByEmail, "asd").Return(int64(0), errors.New("Indistinct"))
+	sent, err := svc.ResendEmailConfirmationEmail("asd")
+	require.False(t, sent)
+	require.Equal(t, "Indistinct", err.Error())
+}
+
+func TestResendEmailConfirmationEmailShouldReturnErrorWhenSendConfirmationEmailFaisl(t *testing.T) {
+	svc, mock := GetServiceAndMock()
+	mock.On(GetuserIdByEmail, "asd").Return(int64(1), nil)
+	verified := &UserEmail{
+		VerfiedEmail: false,
+	}
+	mock.On(GetEmailByUserId, int64(1)).Return("notempty", verified, nil)
+	sent, err := svc.ResendEmailConfirmationEmail("asd")
+	require.False(t, sent)
+	require.Equal(t, config.ErrEmailSendServiceNotWorking, err)
 }
