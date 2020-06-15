71efb47fc6696d3109bbcf141e3c1ffd26b0b47a
Nico <nicolasecamilo@gmail.com>
2020-06-15 00:02:58 -0300

    Test

--- internal/encryption/token_test.go
+++ internal/encryption/token_test.go
@@ -9,3 +9,10 @@ func TestGenerateRandomBytes(t *testing.T) {
 	b, _ := GenerateRandomBytes(1)
 	require.NotNil(t, b)
 }
+
+
+func TestGenerateRandomBytesNotPanics(t *testing.T) {
+	require.NotPanics(t, func() {
+		GenerateRandomBytes(1)
+	})
+}
